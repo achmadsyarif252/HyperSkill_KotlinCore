e94a1a4d05e29f2edd22daa3d964ee4ecb4721ef
achmadsyarif252 <19kaizen@gmail.com>
2024-01-14 21:11:47 +0700

    interface inheritance

---
+++ src/main/kotlin/IterfaceInheritance.kt
@@ -0,0 +1,48 @@
+interface AnimalExtends {
+    val numberOfLimbs: Int
+    fun move()
+    fun communicate()
+}
+
+interface BirdExtends : AnimalExtends {
+    val canFly: Boolean
+    fun buildNest()
+}
+
+interface Flying {
+    val flyingSpeed: Int
+    val flyingManeuverability: Int
+}
+
+class ParrotExtends : BirdExtends, Flying {
+    override val flyingManeuverability: Int
+        get() = 95
+    override val numberOfLimbs: Int
+        get() = 2
+    override val canFly: Boolean
+        get() = true
+    override val flyingSpeed: Int
+        get() = 20
+
+    override fun move() {
+        fly()
+    }
+
+    override fun communicate() {
+        speak()
+    }
+
+    override fun buildNest() {
+        collectionMaterials()
+        findGoodPlace()
+        buildSmallNest()
+    }
+}
+
+interface Insect:Flying{
+
+}
+
+class Fly:Insect,AnimalExtends{
+
+}
